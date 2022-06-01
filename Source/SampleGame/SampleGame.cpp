453daa1fc930559b4909d0c00f8ef1df08658c6f
Pavel Zhogolev <pavel.zhogolev@sperasoft.com>
2022-06-01 23:00:05 +0300

    Initial sample game.

---
+++ Source/SampleGame/SampleGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SampleGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SampleGame, "SampleGame" );
