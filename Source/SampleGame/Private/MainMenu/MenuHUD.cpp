bc91816a319d1c5b85a7ce409e122adc4d6bcd60
korney.tertychnyi <korney.tertychnyi@sperasoft.com>
2022-11-25 15:46:34 +0100

    [+] Set app active and foreground when start on Windows

--- Source/SampleGame/Private/MainMenu/MenuHUD.cpp
+++ Source/SampleGame/Private/MainMenu/MenuHUD.cpp
@@ -7,26 +7,43 @@
 #include "Widgets/SWeakWidget.h"
 #include "Engine/Engine.h"
 
+#if PLATFORM_WINDOWS
+#include "Windows/AllowWindowsPlatformTypes.h"
+#include "Windows.h"
+#endif
+
 void AMenuHUD::BeginPlay()
 {
 	Super::BeginPlay();
+
+#if PLATFORM_WINDOWS
+	const HWND GameHWND = (HWND)GEngine->GameViewport->GetWindow()->GetNativeWindow()->GetOSWindowHandle();
+
+	::SetWindowPos(GameHWND, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+	::SetWindowPos(GameHWND, HWND_NOTOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+	::SetForegroundWindow(GameHWND);
+	::SetCapture(GameHWND);
+	::SetFocus(GameHWND);
+	::SetActiveWindow(GameHWND);
+#endif
+
 	ShowMainMenu();
 	SetInputModeToUI();
 }
 
 void AMenuHUD::ShowMainMenu()
 {
-	if(GEngine && GEngine->GameViewport)
+	if (GEngine && GEngine->GameViewport)
 	{
 		MenuWidget = SNew(SMainMenuWidget).OwningHUD(this);
 		GEngine->GameViewport->AddViewportWidgetContent(SAssignNew(MenuWidgetContainer, SWeakWidget)
 			.PossiblyNullContent(MenuWidget.ToSharedRef()));
- 	}
+	}
 }
 
 void AMenuHUD::HideMainMenu()
 {
-	if(GEngine && GEngine->GameViewport && MenuWidgetContainer.IsValid())
+	if (GEngine && GEngine->GameViewport && MenuWidgetContainer.IsValid())
 	{
 		GEngine->GameViewport->RemoveViewportWidgetContent(MenuWidgetContainer.ToSharedRef());
 	}
@@ -34,7 +51,7 @@ void AMenuHUD::HideMainMenu()
 
 void AMenuHUD::ShowSettingsMenu()
 {
-	if(GEngine && GEngine->GameViewport)
+	if (GEngine && GEngine->GameViewport)
 	{
 		SettingsMenuWidget = SNew(SSettingsMenuWidget).OwningHUD(this);
 		GEngine->GameViewport->AddViewportWidgetContent(SAssignNew(MenuWidgetContainer, SWeakWidget)
@@ -44,7 +61,7 @@ void AMenuHUD::ShowSettingsMenu()
 
 void AMenuHUD::HideSettingsMenu()
 {
-	if(GEngine && GEngine->GameViewport && MenuWidgetContainer.IsValid())
+	if (GEngine && GEngine->GameViewport && MenuWidgetContainer.IsValid())
 	{
 		GEngine->GameViewport->RemoveViewportWidgetContent(SettingsMenuWidget.ToSharedRef());
 	}
@@ -52,7 +69,7 @@ void AMenuHUD::HideSettingsMenu()
 
 void AMenuHUD::SetInputModeToUI()
 {
-	if(PlayerOwner)
+	if (PlayerOwner)
 	{
 		PlayerOwner->bShowMouseCursor = true;
 		PlayerOwner->SetInputMode(FInputModeUIOnly());
